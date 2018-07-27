3ca0807926453aeb745fb86b76b30d914b75dfd2
Icy <Khaledyasser95@gmail.com>
2018-07-27 16:14:33 -0500

    Clean

---
+++ Source/S_5_TesingGrounds/ThirdPersonCharacter.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TP_ThirdPerson/TP_ThirdPersonGameMode.h"
+#include "ThirdPersonCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class S_5_TESINGGROUNDS_API AThirdPersonCharacter : public ATP_ThirdPersonGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+	
+};
